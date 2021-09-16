9b769b618a220c40797b1aa38a52d1b8655b09b3
Barsha003 <54989585+Barsha003@users.noreply.github.com>
2021-09-16 19:47:10 +0545

    add models in investor

--- spotlightv0/investor/models.py
+++ spotlightv0/investor/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class creator_fund(models.Model):
+    name=models.CharField(max_length=50)
+    email = models.EmailField()
+    address1=models.TextField()
+    address2=models.TextField()
+    zipNo =models.IntegerField()
+    cardName= models.TextField()
+    cardNum=models.IntegerField()
+
