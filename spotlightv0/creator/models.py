25762853a88353f1275aaf60187a0764a7fd4171
Aagan Ubuntu <aaganma@gmail.com>
2021-09-18 11:57:56 +0545

    Category name render fixed in display

--- spotlightv0/creator/models.py
+++ spotlightv0/creator/models.py
@@ -4,16 +4,9 @@ from django.db import models
 
 
 class creator_Basic(models.Model):
-    Creator_Category = (
-        ('1','Art'),
-        ('2', 'Technology'),
-        ('3', 'Fasion'),
-        ('4', 'Food'),
-        ('5', 'Photography'),
-        ('6', 'Others')
-    )
+  
     title = models.CharField(max_length=25)
-    category = models.CharField(max_length=1, choices=Creator_Category)
+    category = models.CharField(max_length=100)
     description=models.TextField()
     image = models.ImageField(upload_to='static/img')
 
