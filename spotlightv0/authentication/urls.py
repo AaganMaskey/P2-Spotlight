c7b68c9c35f79af17e55874f02c21fd8c73a7d58
Aagan Ubuntu <aaganma@gmail.com>
2021-09-18 19:35:23 +0545

    Force login replaced with default homepage

--- spotlightv0/authentication/urls.py
+++ spotlightv0/authentication/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(),name='login'),
     path('logout/', views.LogoutView.as_view(),name='logout'),
     path('activate/<uidb64>/<token>', views.ActivateAccountView.as_view(), name='activate'),
-    path('', login_required(views.HomeView.as_view()),name='home'),
+    path('', views.HomeView.as_view(),name='home'),
     path('request-reset-email', (views.RequestResetEmailView.as_view()), name='request-reset-email'),
     path('set-new-password/<uidb64>/<token>', views.SetNewPasswordView.as_view(), name='set-new-password'),
 ]
