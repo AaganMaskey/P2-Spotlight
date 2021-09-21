c301038879c84c8de4624a43b4a750a4e57fcb1e
Bikalpa Dhakal <bdh@logpoint.com>
2021-09-21 22:35:25 +0545

    Fixed paypal not working. Paypal will now show the amount from DOM "amount" input.

--- spotlightv0/investor/views.py
+++ spotlightv0/investor/views.py
@@ -5,6 +5,7 @@ from django.views.decorators.http import require_POST
 from django.views.generic import View
 from django.contrib.auth.decorators import login_required
 from creator.models import creator_Basic
+from django.views.generic import View
 # Create your views here.
 
 
@@ -13,48 +14,81 @@ def investor(request):
     return render(request, "investor.html")
 
 
-# relative import of forms
-# from .models import creator_Basic, creator_fund
-# from .forms import creatorBasic
+
+def investor_checkout_view(request, pid):
+    if request.method == "POST":
+        error = ""
+        if not request.user.is_authenticated:
+            return redirect('login')
+        if request.method == 'POST':
+            try:
+                amount = float(request.POST['amount'])
+            except:
+                amount = 0
+            projectId = request.POST['projectId']
+            userId = request.POST['userId']
+            userName = request.POST['userName']
+
+            try:
+                investor_fund.objects.create(
+                    pledgeAmount=amount,
+                    userid=userId,
+                    username=userName,
+                    projectid=projectId
+                )
+                obj = creator_Basic.objects.get(pk=projectId)
+                obj.pledgeAmount = obj.pledgeAmount + amount
+                obj.save()
+
+                error = "no"
+            except Exception as e:
+                print(e)
+                error = "yes"
+        p = {'error': error}
+        return render(request, 'investor-checkout.html', p)
+
+    else: 
+        projObj = creator_Basic.objects.get(pk=pid)
+        data = creator_Basic.objects.all().order_by('-TargetLaunchDate')
+        return render(request, "investor-checkout.html", {"fundProject": projObj, "projects": data})  
 
 
-# def InvestorCheckoutView(request):
-    # dictionary for initial data with
-    # field names as keys
-#	class InvestorCheckoutView(View):
-#		def get(self, request, id):
-#				return render(request,"investor-checkout.html",)
+# class InvestorCheckoutView(View):
 
-def InvestorCheckoutView(request, pid):
-    projObj = creator_Basic.objects.get(pk=pid)
-    data = creator_Basic.objects.all().order_by('-TargetLaunchDate')
-    return render(request, "investor-checkout.html", {"fundProject": projObj, "projects": data})
+#     def get(self, request, *args, **kwargs):
+#         pid = kwargs["pid"]
+#         projObj = creator_Basic.objects.get(pk=pid)
+#         data = creator_Basic.objects.all().order_by('-TargetLaunchDate')
+#         return render(request, "investor-checkout.html", {"fundProject": projObj, "projects": data})
 
+    
+#     def post(self, request, *args, **kwargs):
+#         error = ""
+#         if not request.user.is_authenticated:
+#             return redirect('login')
+#         if request.method == 'POST':
+#             try:
+#                 amount = float(request.POST['amount'])
+#             except:
+#                 amount = 0
+#             projectId = request.POST['projectId']
+#             userId = request.POST['userId']
+#             userName = request.POST['userName']
 
-def AddFund(request):
-    error = ""
-    if not request.user.is_authenticated:
-        return redirect('login')
-    if request.method == 'POST':
-        amount = request.POST['amount']
-        projectId = request.POST['projectId']
-        userId = request.POST['userId']
-        userName = request.POST['userName']
+#             try:
+#                 investor_fund.objects.create(
+#                     pledgeAmount=amount,
+#                     userid=userId,
+#                     username=userName,
+#                     projectid=projectId
+#                 )
+#                 obj = creator_Basic.objects.get(pk=projectId)
+#                 obj.pledgeAmount = obj.pledgeAmount + amount
+#                 obj.save()
 
-        try:
-            investor_fund.objects.create(
-                pledgeAmount=amount,
-                userid=userId,
-                username=userName,
-                projectid=projectId
-            )
-            obj = creator_Basic.objects.get(pk=projectId)
-            obj.pledgeAmount = obj.pledgeAmount + amount
-            obj.save()
-
-            error = "no"
-        except Exception as e:
-            print(e)
-            error = "yes"
-    p = {'error': error}
-    return render(request, 'investor-checkout.html', p)
+#                 error = "no"
+#             except Exception as e:
+#                 print(e)
+#                 error = "yes"
+#         p = {'error': error}
+#         return render(request, 'investor-checkout.html', p)
