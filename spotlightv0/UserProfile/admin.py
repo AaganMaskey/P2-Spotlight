1b4ec87076d52fa3dd7d90b66e78a1d96a41783c
Barsha003 <54989585+Barsha003@users.noreply.github.com>
2021-09-22 22:54:58 +0545

    profile DB added

--- spotlightv0/UserProfile/admin.py
+++ spotlightv0/UserProfile/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from UserProfile.models import profile_Basic
+
+
+class profileAdmin(admin.ModelAdmin):
+    list_display = ['User','Email', 'Phone', 'Address', 'Funded',
+                    'Projects']
+
+
+admin.site.register(profile_Basic, profileAdmin)
+
+
+
+    
--- spotlightv0/UserProfile/models.py
+++ spotlightv0/UserProfile/models.py
@@ -7,7 +7,7 @@ class profile_Basic(models.Model):
     User = models.CharField(max_length=25)
     Email = models.EmailField(max_length=25)
     Phone = models.IntegerField(default='0')
-    Adress = models.CharField(max_length=25)
+    Address = models.CharField(max_length=25)
     Funded = models.IntegerField(default='0')
     Projects = models.CharField(max_length=100)
     
